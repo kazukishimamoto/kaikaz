7560e95484b4f84b3817b6a78b4f6387b58d2ab2
Kazuki Shimamoto <jakky0529@gmail.com>
2021-03-26 07:28:40 +0900

    ファイル送信画面作成とlint

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css'}
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' }
     ]
   },
 
--- pages/images/new.vue
+++ pages/images/new.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="container">
-    <div class="file">
+    <div class="file has-name">
       <label class="file-label">
-        <input class="file-input" type="file" name="resume">
+        <input class="file-input" type="file" name="resume" @change="onFileChange">
         <span class="file-cta">
           <span class="file-icon">
-            <i class="fas fa-upload"></i>
+            <i class="fas fa-upload" />
           </span>
           <span class="file-label">
             Choose a file…
           </span>
         </span>
+        <span class="file-name">
+          {{ imageName }}
+        </span>
       </label>
     </div>
+    <button class="button submit" @click="submit">
+      送信
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      imageName: 'No file'
+    }
+  },
+  methods: {
+    onFileChange (e) {
+      const files = e.target.files || e.dataTransfer.files
+      this.imageName = files[0].name
+    },
+    submit () {
+      console.log('ファイルアップロード！')
+    }
+  }
 }
 </script>
 
@@ -27,8 +46,13 @@ export default {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+
+.submit {
+  margin-top: 10px;
 }
 </style>

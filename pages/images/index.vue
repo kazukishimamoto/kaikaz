c2429ecde486c0a4475df4c823e45625d4ebdf38
Kazuki Shimamoto <jakky0529@gmail.com>
2021-04-10 12:43:40 +0900

    画像一覧のレイアウト調整

--- pages/images/index.vue
+++ pages/images/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
+  <div class="container">
     <h1>画像一覧</h1>
-    <p>{{ imageNames }}</p>
-
-    <div class="columns">
-      <img v-for="url in urls" :key="url" :src="url" class="image column">
+    <div v-for="(images,y) in imageTable" :key="y" class="tile is-ancestor">
+      <div  class="tile is-parent">
+        <div v-for="(image,x) in images" :key="image" class="tile is-child box">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image">
+                <img :src="image" :alt="image">
+              </figure>
+            </div>
+          </div>
+          <label>{{ imageNames[x+y*3] }}</label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +38,6 @@ export default {
 
     // 画像一覧取得
     this.imageNames.forEach((imgName) => {
-      console.log('hoge')
       listRef.child(`${imgName}`).getDownloadURL().then((url) => {
         const xhr = new XMLHttpRequest()
         xhr.onload = () => {
@@ -41,13 +49,37 @@ export default {
         this.urls.push(url)
       })
     })
+  },
+  computed: {
+    imageTable() {
+      const imageTable = []
+
+      if (this.urls) {
+        for (let y = 0; y < this.imageNames.length/3; y++) {
+          const tmp = []
+          for (let x = 0; x < 3; x++) {
+            tmp.push(this.urls[x+y*3])
+          }
+          imageTable.push(tmp)
+        }
+        return imageTable
+      }
+
+      return imageTable
+    }
   }
 }
 </script>
 
 <style scoped>
-img {
-  width: 100px;
-  margin-right: 10px;
+figure img {
+  width: 480px;
+  height: 240px;
+  object-fit: contain;
+}
+
+label {
+  text-align: center;
+  display: block;
 }
 </style>
